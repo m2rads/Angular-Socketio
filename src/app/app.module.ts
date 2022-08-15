4f15d91061b53e62ccee658d2d6050167464e7c3
m2rads <argo.mohrad@gmail.com>
2022-08-14 19:36:25 -0700

    add pick-username component to choose a username

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PickUsernameComponent } from './components/pick-username/pick-username.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+
+const config: SocketIoConfig = {
+  url: environment.socketUrl, // socket server url;
+  options: {
+    transports: ['websocket'],
+  },
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PickUsernameComponent
-  ],
+  declarations: [AppComponent, PickUsernameComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SocketIoModule.forRoot(config),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/pick-username/pick-username.component.ts
+++ src/app/components/pick-username/pick-username.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-pick-username',
   templateUrl: './pick-username.component.html',
-  styleUrls: ['./pick-username.component.css']
+  styleUrls: ['./pick-username.component.css'],
 })
 export class PickUsernameComponent implements OnInit {
+  singupForm = this.formBuilder.group({
+    name: '',
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private socket: Socket) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // process picking username here
+    console.log(
+      'You have successfully picked a name',
+      this.singupForm.value.name
+    );
+    this.socket.emit('input', this.singupForm.value.name);
+    this.singupForm.reset();
   }
-
 }
