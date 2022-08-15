f25227f3ffcfeb76ead1fb75a7777890c9ed00ba
m2rads <argo.mohrad@gmail.com>
2022-08-14 21:57:15 -0700

    add chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+interface User {
+  username: String;
+  self: Boolean;
+  connected: Boolean;
+  hasNewMessages: Boolean;
+}
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
+  users: Array<User>;
+  selectedUser: any;
+  constructor(private socket: Socket) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelectUser(user: User): void {
+    this.selectedUser = user;
+    user.hasNewMessages = false;
   }
-
 }
