eb2aa73cc8bf48c732a149e2691e410cacc1615c
m2rads <argo.mohrad@gmail.com>
2022-08-14 18:11:47 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PickUsernameComponent } from './components/pick-username/pick-username.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PickUsernameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pick-username/pick-username.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pick-username',
+  templateUrl: './pick-username.component.html',
+  styleUrls: ['./pick-username.component.css']
+})
+export class PickUsernameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
