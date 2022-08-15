f8f5ee819cb69934d0cd71a89a084b11a4e37435
m2rads <argo.mohrad@gmail.com>
2022-08-14 20:24:28 -0700

    update: online status

--- src/app/components/status-icon/status-icon.component.ts
+++ src/app/components/status-icon/status-icon.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-status-icon',
   templateUrl: './status-icon.component.html',
-  styleUrls: ['./status-icon.component.css']
+  styleUrls: ['./status-icon.component.css'],
 })
 export class StatusIconComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  connected: Boolean;
+  constructor() {
+    this.connected = false;
   }
 
+  ngOnInit(): void {}
 }
